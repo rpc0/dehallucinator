6a94ee154002f614cbfdba183839fcc6c2f66a5b
Kevin Hill <hill.ck@pg.com>
2024-09-17 12:38:27 -0400

    init Flask app.

---
+++ ui/deh/settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Application configuration.
+
+Most configuration is set via environment variables.
+
+For local development, use a .env file to set
+environment variables.
+"""
+from environs import Env
+
+env = Env()
+env.read_env()
+
+ENV = env.str("FLASK_ENV", default="production")
+DEBUG = ENV == "development"
+BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
+DEBUG_TB_ENABLED = DEBUG
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
+
+API_ENDPOINT = "http://api:8080/"
