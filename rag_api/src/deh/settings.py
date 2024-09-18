d55def4877a54350d29225cb5fd61a1925d7fa07
Kevin Hill <hill.ck@pg.com>
2024-09-18 10:24:50 -0400

    Fix for internal docker network routing.

--- rag_api/src/deh/settings.py
+++ rag_api/src/deh/settings.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATA_FOLDER = os.environ.get("DATA_FOLDER", "data")
-OLLAMA_HOST = os.environ.get("OLLAMA_HOST", "http://localhost:7869")
+DATA_FOLDER = os.environ.get("DATA_FOLDER", "/data")
+OLLAMA_HOST = os.environ.get("OLLAMA_HOST", "http://172.17.0.1:7869")
 LLM_MODEL = os.environ.get("LLM_MODEL", "llava-phi3:latest")
