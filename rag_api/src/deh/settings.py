b5406d6daca53d6d02f29256d8bad324b3791924
Kevin Hill <hill.ck@pg.com>
2024-09-19 14:22:12 -0400

    Added additional params to API results.

--- rag_api/src/deh/settings.py
+++ rag_api/src/deh/settings.py
@@ -7,4 +7,11 @@ load_dotenv()
 
 DATA_FOLDER = os.environ.get("DATA_FOLDER", "/data/contexts")
 OLLAMA_HOST = os.environ.get("OLLAMA_HOST", "http://172.17.0.1:7869")
+
 LLM_MODEL = os.environ.get("LLM_MODEL", "llava-phi3:latest")
+EMBEDDING_MODEL = os.environ.get(
+    "EMBEDDING_MODEL", "sentence-transformers/all-mpnet-base-v2"
+)
+
+TXT_CHUNK_SIZE = int(os.environ.get("TXT_CHUNK_SIZE", "1500"))
+TXT_CHUNK_OVERLAP = int(os.environ.get("TXT_CHUNK_OVERLAP", "100"))
