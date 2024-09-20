16dfad75f34862ef8f03454ddb0ba1a0a25ab8d8
Kevin Hill <hill.ck@pg.com>
2024-09-20 10:20:11 -0400

    Docker creation and dl folder creation fixes.

--- measurement/src/deh/dl/__init__.py
+++ measurement/src/deh/dl/__init__.py
@@ -1,5 +1,7 @@
 from deh.assessment import QASet, Context
 from typing import List
+from pathlib import Path
+import os
 
 
 class AssessmentDataDownloader:
@@ -12,9 +14,15 @@ class AssessmentDataDownloader:
         """Save context files to directory path.
         - dir_path: directory to save context files
         """
+
+        # Create Folder if does not exist:
+        if not os.path.exists(dir_path):
+            print(f"Warning: Path {dir_path} does not exist and will be created.")
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
+
         context_count = 1
         for context in self._contexts:
-            with open(f"{dir_path}context_{context_count}.context", "w") as f:
+            with open(f"{dir_path}/context_{context_count}.context", "w") as f:
                 f.write(context)
                 context_count = context_count + 1
 
@@ -22,6 +30,14 @@ class AssessmentDataDownloader:
         """Save qaset file to diretory path.
         - file_path: file to save question-answer sets
         """
+
+        # Create Folder if does not exist:
+        p = Path(file_path)
+        if not os.path.exists(p.parent):
+            print(f"Warning: Path {p.parent} does not exist and will be created.")
+
+        Path(p.parent).mkdir(parents=True, exist_ok=True)
+
         with open(f"{file_path}", "w") as f:
             for qas in self._qaset:
                 f.write(f"{qas.question}\t{qas.ground_truth}\n")
