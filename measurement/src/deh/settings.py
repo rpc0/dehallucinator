09203510b8b1adccb58471928156f52ea0d0ca46
Kevin Hill <hill.ck@pg.com>
2024-09-27 12:23:29 -0400

    Update default models.

--- measurement/src/deh/settings.py
+++ measurement/src/deh/settings.py
@@ -7,7 +7,9 @@ load_dotenv()
 
 API_ANSWER_ENDPOINT = os.environ.get("API_ANSWER_ENDPOINT", "localhost/api/")
 OLLAMA_URL = os.environ.get("OLLAMA_URL", "http://localhost:7869")
-ASSESSMENT_LLM_MODEL = os.environ.get("ASSESSMENT_LLM_MODEL", "tinyllama:latest")
+ASSESSMENT_LLM_MODEL = os.environ.get(
+    "ASSESSMENT_LLM_MODEL", "llama3.1:8b-instruct-q3_K_L"
+)
 ASSESSMENT_EMBEDDING_MODEL = os.environ.get(
-    "ASSESSMENT_EMBEDDING_MODEL", "all-minilm:latest"
+    "ASSESSMENT_EMBEDDING_MODEL", "mxbai-embed-large:latest"
 )
