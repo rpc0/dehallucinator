3bdc5239cb2dbb903fa734c1c21f2f22d1c7728b
Kevin Hill <hill.ck@pg.com>
2024-09-28 08:55:19 -0400

    Using local prompt vs. remote hub pull

--- rag_api/main.py
+++ rag_api/main.py
@@ -22,7 +22,11 @@ import logging
 
 import deh.settings as settings
 from deh.utils import format_context_documents as format_docs
-from deh.prompts import qa_eval_prompt_with_context_text, LLMEvalResult
+from deh.prompts import (
+    qa_eval_prompt_with_context_text,
+    LLMEvalResult,
+    rag_prompt_llama_text,
+)
 
 app = FastAPI()
 
@@ -142,7 +146,7 @@ async def answer(question: str):
         callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
     )
 
-    response = rag_chain_with_llm_context_self_evaluation(retriever, question, llm)
+    response = basic_rag_chain_with_context(retriever, question, llm)
     return {
         "response": response,
         # Diagnostic values used for measurement logging, etc:
@@ -177,7 +181,9 @@ def basic_rag_chain(retriever, question, llm):
 def basic_rag_chain_with_context(retriever, question, llm):
     """Simplest RAG Chain (v0.1) implementation which includes context pass-through."""
 
-    qa_prompt = hub.pull(settings.LLM_PROMPT)
+    qa_prompt = PromptTemplate(
+        template=rag_prompt_llama_text, input_variables=["question", "context"]
+    )
 
     # fmt: off
     rag_chain = (
