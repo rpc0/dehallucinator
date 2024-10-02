bd1552dce9267e09577dcfff56398e1d514db616
Kevin Hill <hill.ck@pg.com>
2024-10-02 08:29:16 -0400

    Linting fix.

--- rag_api/src/deh/settings.py
+++ rag_api/src/deh/settings.py
@@ -9,7 +9,9 @@ DATA_FOLDER = os.environ.get("DATA_FOLDER", "/data/contexts")
 OLLAMA_HOST = os.environ.get("OLLAMA_HOST", "http://172.17.0.1:7869")
 
 LLM_MODEL = os.environ.get("LLM_MODEL", "llama3.1:8b-instruct-q3_K_L")
-LLM_PROMPT = os.environ.get("LLM_PROMPT", "rlm/rag-prompt-llama") #https://smith.langchain.com/hub/rlm/rag-prompt-llama
+LLM_PROMPT = os.environ.get(
+    "LLM_PROMPT", "rlm/rag-prompt-llama"
+)  # https://smith.langchain.com/hub/rlm/rag-prompt-llama
 
 EMBEDDING_MODEL = os.environ.get(
     "EMBEDDING_MODEL", "sentence-transformers/all-mpnet-base-v2"
