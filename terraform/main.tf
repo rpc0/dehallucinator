c1b8fb9690aa6ca0d15457ab502091b0158ed9cd
Kevin Hill <hill.ck@pg.com>
2024-10-11 11:11:29 -0400

    Inbound for 80 and install dependencies.

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,12 +45,19 @@ resource "aws_subnet" "dev_host_subnet" {
 resource "aws_security_group" "dev_host" {
   name   = "deh-dev-host"
   vpc_id = aws_vpc.default.id
-  ingress {
+  ingress = [ {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  },
+  {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  ]
   egress {
     from_port   = 0
     to_port     = 0
