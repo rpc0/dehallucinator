172dac8f6f1a15546493f2038bc04b4e7f3780cb
Kevin Hill <ckhill@gmail.com>
2024-12-13 01:28:57 +0000

    experiment params in API

--- measurement/src/deh/settings.py
+++ measurement/src/deh/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ANSWER_ENDPOINT = os.environ.get("API_ANSWER_ENDPOINT", "localhost/api/")
 OLLAMA_URL = os.environ.get("OLLAMA_URL", "http://localhost:7869")
 ASSESSMENT_LLM_MODEL = os.environ.get(
-    "ASSESSMENT_LLM_MODEL", "llama3.1:8b-instruct-q3_K_L"
+    "ASSESSMENT_LLM_MODEL", "llama3.1:8b-instruct-q3_K_L" # qwen2.5:7b
 )
 ASSESSMENT_EMBEDDING_MODEL = os.environ.get(
     "ASSESSMENT_EMBEDDING_MODEL", "mxbai-embed-large:latest"
